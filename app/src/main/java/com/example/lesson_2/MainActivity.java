e2d0a831a8d44416fe1f86677d7e0a287d6685f0
arzy6e4 <abdyva1yevarzy6ek@gmail.com>
2023-03-03 19:46:41 +0600

    Initial commit

---
+++ app/src/main/java/com/example/lesson_2/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.lesson_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.edit_text);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT,editText.getText().toString());
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+
+            }
+        });
+    }
+}
